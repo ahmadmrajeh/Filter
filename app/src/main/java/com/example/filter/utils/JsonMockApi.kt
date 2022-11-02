6f33130b7b8f2fea5bb63a1acb20a7fb3261d90f
Admin <ahmad_rh2010@hotmail.com>
2022-11-02 17:53:45 +0300

    j

---
+++ app/src/main/java/com/example/filter/utils/JsonMockApi.kt
@@ -0,0 +1,27 @@
+package com.example.filter.utils
+import android.content.Context
+import android.util.Log
+import java.io.IOException
+
+
+class JsonMockApi {
+
+   companion object {
+       fun getJsonDataFromAsset(context: Context, fileName: String): String? {
+           val jsonString: String
+           try {
+               jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+               Log.e("jsss","try success ")
+           } catch (ioException: IOException) {
+               ioException.printStackTrace()
+               Log.e("jsss","try Failed "+ioException.message)
+               Log.e("jsss","  Failed cause"+ioException.cause)
+               return null
+           }
+           return jsonString
+       }
+
+   }
+
+
+}
